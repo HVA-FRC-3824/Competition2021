f1f108366c22b742b234f5bc75068c99e4aa2e5b
jyoshioka <programmingsensei@gmail.com>
2020-01-19 21:21:44 -0500

    Created new project with WPILib template. Added subsystems folder, commands folder, and OI.java.

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+public class OI {
+
+    /**
+     * Declare all joysticks and buttons here
+     * Example: public Joystick myJoystick;
+     */
+
+
+    public OI() {
+        /**
+         * Instantiate declared joysticks and buttons here
+         * Example: myJoystick = new Joystick(0);
+         */
+    }
+
+}
