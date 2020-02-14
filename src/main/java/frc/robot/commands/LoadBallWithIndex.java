b4c8bd52ac768bdec16bf912e12963f58e6ddcb7
jyoshioka <programmingsensei@gmail.com>
2020-02-14 17:52:52 -0500

    started intake code with index befre jovi needed the laptop

---
+++ src/main/java/frc/robot/commands/LoadBallWithIndex.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class LoadBallWithIndex extends CommandBase 
+{
+  // The subsystem the command runs on
+
+  public LoadBallWithIndex()
+  {
+    //addRequirements(m_subsystem);
+  }
+
+  @Override
+  public void initialize()
+  {
+  }
+  
+  @Override
+  public void execute()
+  {
+      // start motor
+      // Add ball to count after 
+      
+
+  }
+
+  @Override
+  public void end(boolean interrupted)
+  {
+  }
+
+  @Override
+  public boolean isFinished()
+  {
+    return true;
+  }
+
+
+}
